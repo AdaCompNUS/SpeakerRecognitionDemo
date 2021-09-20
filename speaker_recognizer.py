3ba725a82cafe7426aa9e2549861cfd2f5efa7bc
lyf44 <luyunfan44@gmail.com>
2021-09-20 19:34:39 +0800

    Incorporate into RLS architecutre <2>

--- speaker_recognizer.py
+++ speaker_recognizer.py
@@ -44,8 +44,8 @@ class SpeakerRecognizer():
         print("[SpeakerRecognizer]: remembered new speaker {}!!".format(speaker_name))
         return True
 
-    def recognize(self, filename):
-        audio_feat = self.loadWAV(osp.join(CUR_DIR, "../audio_file/{}".format(filename)))
+    def recognize_audio(self, audio_feat):
+        audio_feat = torch.FloatTensor(audio_feat)
         score_matrix = {}
         feat_test = self.model(audio_feat).detach()
         feat_test = torch.nn.functional.normalize(feat_test, p=2, dim=1)
@@ -62,6 +62,23 @@ class SpeakerRecognizer():
 
         return top_speaker, score
 
+    def recognize_file(self, filename):
+        audio_feat = self.loadWAV(osp.join(CUR_DIR, "../audio_file/{}".format(filename)))
+        score_matrix = {}
+        feat_test = self.model(audio_feat).detach()
+        feat_test = torch.nn.functional.normalize(feat_test, p=2, dim=1)
+        for enroll_audio in self.utt_enroll_dict.keys():
+            feat_enroll = self.model(self.utt_enroll_dict[enroll_audio]).detach()
+            feat_enroll = torch.nn.functional.normalize(feat_enroll, p=2, dim=1)
+            score = [float(numpy.round(- torch.nn.functional.pairwise_distance(feat_enroll.unsqueeze(-1), feat_test.unsqueeze(-1).transpose(0,2)).detach().numpy(), 4))]
+            score_matrix[enroll_audio.split('/')[-1].split('.')[0]] = score
+
+        score_sorted = sorted(score_matrix.items(), key=lambda item: item[1], reverse = True)
+        top_speaker, score = score_sorted[0]
+        score = score[0]
+        print("[SpeakerRecognizer]: top_speaker: {}, score: {}".format(top_speaker, score))
+
+        return top_speaker, score
 
 if __name__ == '__main__':
     recog = SpeakerRecognizer()
